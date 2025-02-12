06cb74dd7219f69f93e9f6749cb5496e7277c666
SPBCodes <git@spbcodes.online>
2025-02-12 08:01:57 +0000

    Create staffrecord.php

---
+++ tabs/staffrecord.php
@@ -0,0 +1,5 @@
+<h1>Example of data passed to file</h1><p>The data sent with the ajax request is shown below</p>
+<pre><?
+	print_r($_POST);
+?></pre>
+<p>A dynamic page could use this data as a database ID lookup to provide specific data about this staff member, for example.</p>
